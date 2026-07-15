98290d6e92f1d234bbbc37f0f1fe6e8e1d89fc0a
192524471simats-rosey <192524471.simats@saveetha.com>
2026-07-15 13:39:02 +0530

    Create exp9.c

---
+++ exp9.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[20],n,i,key,low,high,mid;
+
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+
+    printf("Enter sorted elements:\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    printf("Enter key: ");
+    scanf("%d",&key);
+
+    low=0;
+    high=n-1;
+
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+
+        if(a[mid]==key)
+        {
+            printf("Element found at position %d",mid+1);
+            return 0;
+        }
+        else if(key>a[mid])
+            low=mid+1;
+        else
+            high=mid-1;
+    }
+
+    printf("Element not found");
+
+    return 0;
+}
